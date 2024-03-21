ee02f90b640d0a270aafa9af16778c02d84c77e8
shehryarali8709 <shehryarali8709@gmail.com>
2024-03-22 01:35:39 +0500

    fixes

--- src/app/recipients/recipients.component.ts
+++ src/app/recipients/recipients.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipients',
   standalone: true,
-  imports: [],
+  imports: [RadioButtonModule,FormsModule],
   templateUrl: './recipients.component.html',
   styleUrl: './recipients.component.sass'
 })
 export class RecipientsComponent {
+  recipientType: string | null = null; 
+  logSelectedValue(value: string | null): void {
+    console.log('Selected Value:', value);
+  }
 
 }
