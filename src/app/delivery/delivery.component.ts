003e86f90c7f1ecedaea9ef24063c8196e214a63
shehryarali8709 <shehryarali8709@gmail.com>
2024-03-24 17:40:55 +0500

    complete

--- src/app/delivery/delivery.component.ts
+++ src/app/delivery/delivery.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, Directive,Input ,OnInit} from '@angular/core';
 import { CalendarModule } from 'primeng/calendar';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,FormControl, FormGroup } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CommonModule } from '@angular/common'; 
+import { InputSwitchModule } from 'primeng/inputswitch';
+import {MatIconModule} from '@angular/material/icon';
+import { DropdownModule } from 'primeng/dropdown';
+interface Timezone {
+  timezone: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-delivery',
   standalone: true,
-  imports: [CalendarModule,FormsModule],
+  imports: [DropdownModule,CalendarModule,FormsModule,RadioButtonModule,MatIconModule,CommonModule,InputSwitchModule],
   templateUrl: './delivery.component.html',
-  styleUrl: './delivery.component.sass'
+  styleUrl: './delivery.component.sass',
 })
-export class DeliveryComponent {
+export class DeliveryComponent implements OnInit  {
   date: Date | undefined;
+  checked: boolean = true;
+  timezone: Timezone[] | undefined;
+  selectedTimezone: Timezone | undefined;
+  calendarVal1?: Date; 
+  calendarVal2?: Date; 
+  DeliveryType: string | null = 'Current/Past Delivery'; 
+  logSelectedValue(value: string | null): void {
+    console.log('Selected Value:', value);
+  }
+  activeSection: number | null = 0;
+  settings = [
+    { name: 'Email Setting' },
+    { name: 'Completion Setting' },
+    { name: 'Custom Mail Server' },
+    { name: 'Tracking Setting' }
+  ];
+
+  toggleSection(index: number) {
+    console.log('works')
+    if (this.activeSection === index) {
+      this.activeSection = null; 
+    } else {
+      this.activeSection = index;
+    }
+  }
+  ngOnInit() {
+    this.timezone = [
+        { timezone: "UTC (Coordinated Universal Time)", code: "UTC" },
+        { timezone: "GMT (Greenwich Mean Time)", code: "GMT" },
+        { timezone: "EST (Eastern Standard Time)", code: "EST" },
+        { timezone: "EDT (Eastern Daylight Time)", code: "EDT" },
+        { timezone: "CST (Central Standard Time)", code: "CST" },
+        { timezone: "CDT (Central Daylight Time)", code: "CDT" },
+        { timezone: "MST (Mountain Standard Time)", code: "MST" },
+        { timezone: "MDT (Mountain Daylight Time)", code: "MDT" },
+        { timezone: "PST (Pacific Standard Time)", code: "PST" },
+        { timezone: "PDT (Pacific Daylight Time)", code: "PDT" },
+      
+          ];
+}
 
 }
--- src/app/recipients/recipients.component.ts
+++ src/app/recipients/recipients.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild,ElementRef} from '@angular/core';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import { FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipients',
   standalone: true,
-  imports: [RadioButtonModule,FormsModule],
+  imports: [RadioButtonModule,FormsModule,CommonModule,MatIconModule,ReactiveFormsModule],
   templateUrl: './recipients.component.html',
   styleUrl: './recipients.component.sass'
 })
+
 export class RecipientsComponent {
-  recipientType: string | null = null; 
+  recipientType: string | null = 'CSV/Excel'; 
+  showCancelIcon: boolean = false;
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  showError: boolean = false;
+  emailList: string[] = [];
   logSelectedValue(value: string | null): void {
     console.log('Selected Value:', value);
   }
+  @ViewChild('emailInput') emailInput! : ElementRef;
 
+    constructor() {}
+    onInputChange(): void {
+      this.showCancelIcon = this.emailInput.nativeElement.value.length > 0;
+    }
+  
+    clearInput(): void {
+      this.emailInput.nativeElement.value = '';
+      this.showCancelIcon = false;
+    }
+    onEnter(): void {
+      if (this.emailFormControl.valid) {
+        const emailValue: string | null = this.emailFormControl.value;
+        if (emailValue !== null) {
+          this.emailList.push(emailValue);
+          this.emailFormControl.reset();
+        }
+      } else {
+        this.showError = true;
+      }
+    }
+    
 }
